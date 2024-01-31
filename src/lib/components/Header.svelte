92a776c36158fc6ce92e5b491693b0b42660c420
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2024-01-31 18:24:27 +0100

    added icons to header links

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
-	import { fade, fly, slide } from 'svelte/transition';
+	import { fade, slide } from 'svelte/transition';
 	import Button from './Button.svelte';
 	import ThemeSelector from './ThemeSelector.svelte';
-	import { flip } from 'svelte/animate';
 
 	export let y: number;
 
 	let tabs = [
 		{
+			href: '/',
+			text: 'Home',
+			icon: 'fa-solid fa-home',
+			hideInHeader: true
+		},
+		{
 			href: '/projects',
-			text: 'Projects'
+			text: 'Projects',
+			icon: 'fa-solid fa-cubes'
 		},
 		{
 			href: '/blog',
-			text: 'Blog'
+			text: 'Blog',
+			icon: 'fa-solid fa-quote-left'
+		},
+		{
+			href: '/contact',
+			text: 'Contact',
+			icon: 'fa fa-envelope',
+			hideInHeader: true
 		}
 	];
 
@@ -30,12 +43,12 @@
 
 <header class="sticky z-10 top-0">
 	<div
-		class={'duration-200 px-6 flex gap-3 item-center items-center border-b-2 border-solid ' +
+		class={'duration-200 px-6 flex item-center items-center border-b-2 border-solid ' +
 			(y > 0 || isDrawerOpen
 				? 'py-4 text-white bg-stone-700 dark:bg-stone-900 border-amber-700 dark:border-amber-900'
 				: 'py-6 border-none border-transparent  ')}
 	>
-		<div class="inline sm:hidden pe-2">
+		<div class="inline sm:hidden pe-4">
 			<button on:click={toggleDrawer} class="text-2xl items-center w-5">
 				{#if isDrawerOpen}
 					<i class="fas fa-times" in:fade></i>
@@ -44,16 +57,18 @@
 				{/if}
 			</button>
 		</div>
-		<h1 class="font-medium h-full">
+		<h1 class="font-medium h-full pe-7">
 			<a href="/" class="link-main duration-200"
 				><b class="font-bold poppins">Alessio</b> Farfaglia</a
 			>
 		</h1>
-		<div class="hidden sm:flex items-center gap-4">
+		<div class="hidden sm:flex items-center gap-5">
 			{#each tabs as tab}
-				<a href={tab.href} class="link-main duration-200">
-					<p>{tab.text}</p>
-				</a>
+				{#if !tab.hideInHeader}
+					<a href={tab.href} class="link-main duration-200">
+						<p><i class={tab.icon}></i> {tab.text}</p>
+					</a>
+				{/if}
 			{/each}
 		</div>
 
@@ -79,42 +94,14 @@
 			out:slide={{ duration: 200 }}
 		>
 			<ul class="flex flex-col gap-3 text-lg">
-				<li>
-					<a href="/" class="link-main duration-200" on:click={closeDrawer}>
-						<p>
-							<i class="fa fa-home pe-2"></i>
-							Home
-						</p>
-					</a>
-				</li>
-				<li>
-					<a href="/projects" class="link-main duration-200" on:click={closeDrawer}>
-						<p>
-							<i class="fa-solid fa-cubes pe-2"></i>
-							<!-- <i class="fa-solid fa-box-archive pe-2"></i> -->
-							<!-- <i class="fa fa-project-diagram pe-2"></i> -->
-							Projects
-						</p>
-					</a>
-				</li>
-				<li>
-					<a href="/blog" class="link-main duration-200" on:click={closeDrawer}>
-						<p>
-							<i class="fa-solid fa-quote-left pe-2"></i>
-							<!-- <i class="fa fa-blog pe-2"></i> -->
-							Blog
-						</p>
-					</a>
-				</li>
-
-				<li>
-					<a href="/contact" class="link-main duration-200" on:click={closeDrawer}>
-						<p>
-							<i class="fa fa-envelope pe-2"></i>
-							Contact
-						</p>
-					</a>
-				</li>
+				{#each tabs as tab}
+					<li>
+						<a href={tab.href} class="link-main duration-200" on:click={closeDrawer}>
+							<i class="{tab.icon} w-7"></i>
+							{tab.text}
+						</a>
+					</li>
+				{/each}
 			</ul>
 			<div class="flex gap-2 ms-auto items-end">
 				<ThemeSelector />
