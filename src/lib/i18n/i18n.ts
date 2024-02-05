fd542208e469973da7b4f5dc186b902e1d7c21b1
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2024-02-05 15:59:40 +0100

    added i18n library and initialization

---
+++ src/lib/i18n/i18n.ts
@@ -0,0 +1,13 @@
+import { init, register } from "svelte-i18n";
+import { browser } from "$app/environment";
+
+const defaultLocale = 'en'
+
+register("en", () => import("./en.json"));
+register("it", () => import("./it.json"));
+
+init({
+	fallbackLocale: defaultLocale,
+	initialLocale: browser ? window.navigator.language : defaultLocale,
+})
+
---
+++ src/routes/locale-test/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { t } from 'svelte-i18n';
+</script>
+
+<h1>{$t('hello')}</h1>
